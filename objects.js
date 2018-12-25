8e85ec00e3d9fc67af4e5b3c64850afdb83bb681
Kaijin Lv <kaijin.lv@gmail.com>
2018-12-25 17:18:23 +0800

    add objects.js

---
+++ objects.js
@@ -0,0 +1,38 @@
+let day1 = {
+    squirrel : false,
+    events:["work","touched tree","pizza","running"]
+};
+console.log(day1.squirrel);
+console.log(day1.wolf);
+day1.wolf=false;
+console.log(day1.wolf);
+
+let descriptions = {
+    work:"Went to work",
+    "touched tree":"Touched a tree"
+};
+
+let anObject = {left:1,right:2};
+console.log(anObject.left);
+delete anObject.left;
+console.log(anObject.left);
+console.log("left" in anObject);
+console.log("right" in anObject);
+
+console.log(Object.keys({x:0,y:0,z:2}));
+
+let objectA = {a:1, b:2};
+Object.assign(objectA, {b:3,c:4});
+    console.log(objectA);
+
+let journal = [
+   { events:["work","touched tree","pizza",
+        "running","television"],
+    squirrel:false},
+   { events:["work","ice cream","cauliflower",
+        "lasagna","touched tree","brushed teeth"],
+    squirrel:false},
+   { events:["weekend","cycling","break",
+        "beer"],
+    squirrel:true}
+];
