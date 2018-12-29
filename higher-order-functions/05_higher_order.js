b3a153f132267849a19b7fb2ae3d29e088b24e9d
Kaijin Lv <kaijin.lv@gmail.com>
2018-12-29 13:38:08 +0800

    add filter and map function

--- higher-order-functions/05_higher_order.js
+++ higher-order-functions/05_higher_order.js
@@ -34,11 +34,34 @@ function textScripts(text) {
     let script = characterScript(char.codePointAt(0));
     return script ? script.name : "none";
   }).filter(({name}) => name != "none");
-
+  
   let total = scripts.reduce((n, {count}) => n + count, 0);
   if (total == 0) return "No scripts found";
-
+  
   return scripts.map(({name, count}) => {
     return `${Math.round(count * 100 / total)}% ${name}`;
   }).join(", ");
 }
+
+require('./scripts.js')
+function filter(array,test){
+  let passed=[];
+  for(let element of array){
+    if(test(element)){
+      passed.push(element);
+    }
+  }
+  return passed;
+}
+
+// console.log(filter(SCRIPTS,script => script.living));
+
+function map(array, transform) {
+  let mapped = [];
+  for(let element of array){
+    mapped.push(transform(element));
+  }
+  return mapped;
+}
+let rtlScripts=SCRIPTS.filter(s=>s.direction=="rtl");
+console.log(map(rtlScripts,s=>s.name));
