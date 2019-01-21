5766bb07262fbfd031a391a2872aeeab757e039f
Kaijin Lv <kaijin.lv@gmail.com>
2019-01-21 10:44:54 +0800

    add solution of excercises

--- ch9-regexp/regexp.js
+++ ch9-regexp/regexp.js
@@ -194,3 +194,22 @@ function verify(regexp, yes, no) {
             console.log(`Unexpected match for '${str}'`);
         }
 }
+
+// Fill in this regular expression.
+let number_1 = /^[+-]?(([0-9]+)\.?([0-9]*)?|([0-9]*)?\.?[0-9]+)([eE][+-]?[0-9]+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+        "1.3e2", "1E-4", "1e+12"
+    ]) {
+    if (!number_1.test(str)) {
+        console.log(`Failed to match '${str}'`);
+    }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+        ".5.", "1f5", "."
+    ]) {
+    if (number_1.test(str)) {
+        console.log(`Incorrectly accepted '${str}'`);
+    }
+}
